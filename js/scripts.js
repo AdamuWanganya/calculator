d616bc82b9794f1607de27a9936a81b7544f9f00
AdamuWanganya <wanganyaadamu@gmail.com>
2021-10-16 21:45:48 +0300

    Add the setup direcories of my project

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-var add = (number1, number2) => number1 + number2;
-
-var number1 = parseInt(prompt("Enter number1: "));
-var number2 = parseInt(prompt("Enter number2: "));
-
-alert(add(number1, number2));
