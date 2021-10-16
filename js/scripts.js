22eb027a18fb0316c0deae9747e475bf000ad5b6
AdamuWanganya <wanganyaadamu@gmail.com>
2021-10-16 22:20:24 +0300

    Grab every botton and stores it in array named buttons and grabs the input box .display and stores it in a constant display

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// select all the buttons
+const buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+const display = document.querySelector('.display');
