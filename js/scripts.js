a771cfbd08cd1691e3d359e66e622d11f37e60cf
AdamuWanganya <wanganyaadamu@gmail.com>
2021-10-16 22:22:27 +0300

    add event listener to each button

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@
 const buttons = document.querySelectorAll('button');
 // select the <input type="text" class="display" disabled> element
 const display = document.querySelector('.display');
+
+// add eventListener to each button
+buttons.forEach(function(button) {
+    button.addEventListener('click', calculate);
+});
